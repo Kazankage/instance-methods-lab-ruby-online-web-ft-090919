10df6e377fc6df447cabd56704f64d2ba46506c6
Randel Jiao <randeljiao@gmail.com>
2019-10-11 06:48:10 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -1,4 +1,19 @@
 class Dog 
   def bark
-    puts ""
+    puts "Woof!"
+  end
+  
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  
+  def walk
+    puts "The Person is walking"
+  end
 end
